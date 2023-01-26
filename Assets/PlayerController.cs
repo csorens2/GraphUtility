8c52f8c784bf3185e041b05aac1b16262fd016f8
BearNBird <csorens2@users.noreply.github.com>
2023-01-26 04:13:12 -0600

    Basic Player Controller

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,17 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Appears in the Inspector view from where you can set the speed
-    public float speed;
-
-    // Rigidbody variable to hold the player ball's rigidbody instance
-    private Rigidbody rb;
+    float heldFrames;
+    Vector3 originalPosition;
+    Vector3 nextPosition;
 
     // Called before the first frame update
     void Start()
     {
-        // Assigns the player ball's rigidbody instance to the variable
-        rb = GetComponent<Rigidbody>();
+        heldFrames = 0;
+        originalPosition = gameObject.transform.position;
+        nextPosition = originalPosition + Vector3.up;
     }
 
     // Called once per frame
@@ -22,14 +21,18 @@ public class PlayerController : MonoBehaviour
     {
         // The float variables, moveHorizontal and moveVertical, holds the value of the virtual axes, X and Z.
 
-        // It records input from the keyboard.
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-
-        // Vector3 variable, movement, holds 3D positions of the player ball in form of X, Y, and Z axes in the space.
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-
-        // Adds force to the player ball to move around.
-        rb.AddForce(movement * speed * Time.deltaTime);
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            heldFrames += 1;
+            if (heldFrames >= 60)
+            {
+                gameObject.transform.position = nextPosition;
+            }    
+        }
+        else
+        {
+            heldFrames = 0;
+            gameObject.transform.position = originalPosition;
+        }
     }
 }
